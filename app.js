cc75ef2946d0d7eddb389b454a6cca14a104196e
foote07 <foote07@gmail.com>
2016-01-11 22:43:47 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+var http      = require('http');
+var express   = require('express');
+
+var app    = express();
+
+app.get('/', function(req, res){
+  res.send("Hello World");
+});
+
+app.use(function(err, req, res, next){
+  if (req.xhr) {
+    res.send(500, 'Something went wrong!');
+  }
+  else {
+    next(err);
+  }
+});
+
+console.log('starting the Express (NodeJS) Web server');
+app.listen(80);
+console.log('Webserver is listening on port 8080');
